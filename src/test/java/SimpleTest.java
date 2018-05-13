40db4ab51cd7a6fae5516ec40bef0c21e8358edc
Kate Lugaskova <lugassyk@gmail.com>
2018-05-13 17:37:07 +0200

    1. create new project 2. Automate small part

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.concurrent.TimeUnit;
+
+public class SimpleTest {
+
+
+    @Test
+    public void navigateToEpamWebSiteSearchAndCheckresult() {
+        System.setProperty("webdriver.chrome.driver", "drv/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.epam.com/");
+
+        WebElement searchButton = driver.findElement(By.cssSelector(".header-search__button.header__utility-button"));
+        searchButton.click();
+        WebElement searchField = driver.findElement(By.cssSelector("..header-search__input"));
+        searchField.sendKeys("Testttttttt");
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+}
+
+
